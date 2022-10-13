70441a6e8c2f796bc10dc079a5c2fd97091a9dd0
Matthew Carter <m@ahungry.com>
2022-10-13 10:41:30 -0400

    Try out responsive layouts

--- ghpages/main.js
+++ ghpages/main.js
@@ -13,11 +13,14 @@ function dosearch()
 }
 
 function snagtags(){
-  const bar = document.querySelector('.bottom-search-bar')
+  const tagbar = document.querySelector('.bottom-search-bar .tags')
+  const distbar = document.querySelector('.bottom-search-bar .dists')
+  const cmpbar = document.querySelector('.bottom-search-bar .cmps')
+  const naybar = document.querySelector('.bottom-search-bar .nays')
   const tags = document.querySelectorAll('img')
   const m = {}
   for (let i = 0; i < tags.length; i++) {
-    const match = tags[i].src.match(/.*(tag-.*?)-/)
+    const match = tags[i].src.match(/.*((tag|dist|cmp|nay)-.*?)-/)
     if (match) {
       const el = document.createElement('img')
       el.src = tags[i].src
@@ -27,19 +30,44 @@ function snagtags(){
     }
   }
   Object.keys(m).forEach(k => {
-    bar.append(m[k])
+    console.log(k)
+    if (/^tag/.test(k))
+      tagbar.append(m[k])
+
+    if (/^dist/.test(k))
+      distbar.append(m[k])
+
+    if (/^cmp/.test(k))
+      cmpbar.append(m[k])
+
+    if (/^nay/.test(k))
+      naybar.append(m[k])
   })
+}
+
+function help() {
+  alert(`ex: 'tag-audio' finds audio mods, 'drizzt' finds any mods that
+mention him, 'cmp-ahungry001' finds mods that ahungry used in tandem, 'dist-g3'
+finds mods from gibberlings3
+`)
 }
 
 function appendsearch() {
   document.body.innerHTML+=`<div class="spacer">&nbsp;</div>
-<div class="bottom-search-bar">tags:
+<div class="bottom-search-bar">
 <input type="text" id="modsearch" />
-<br />
-(ex: 'tag-audio' finds audio mods, 'drizzt' finds any mods that
-mention him, 'cmp-ahungry001' finds mods that ahungry used in tandem, 'dist-g3'
-finds mods from gibberlings3)<br />
-</div>`
+<button class="help" onclick='help()'>help</button>
+<h6>tags</h6>
+<div class="tags overflower"></div>
+<h6>dists</h6>
+<div class="dists overflower"></div>
+<h6>cmps</h6>
+<div class="cmps overflower"></div>
+<h6>nays</h6>
+<div class="nays overflower"></div>
+</div>
+</div>
+`
 }
 
 const links = document.querySelectorAll('a')
